df9300a1316756183cba336f882e7bcd9ca051c2
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-09-15 22:06:59 +0300

    Update Solutions Movies.sql

--- Exercises/Chapter 3/Solutions Movies.sql
+++ Exercises/Chapter 3/Solutions Movies.sql
@@ -5,8 +5,8 @@ SELECT name
 FROM moviestar
 WHERE gender LIKE 'F'
 AND name IN (SELECT name
-			 FROM MOVIEEXEC
-			 WHERE NETWORTH > 10000000);
+	     FROM MOVIEEXEC
+	     WHERE NETWORTH > 10000000);
 
 --2
 SELECT name
@@ -19,13 +19,14 @@ FROM movieexec;
 SELECT title
 FROM movie
 WHERE length > (SELECT length
-				FROM movie
-				WHERE title LIKE 'Star Wars');
+		FROM movie
+		WHERE title LIKE 'Star Wars');
 
 --4
 SELECT m.title, e.name
 FROM movieexec e
 JOIN movie m ON e.cert# = m.producerc#
 WHERE e.networth > (SELECT networth
-					FROM movieexec
-					WHERE name LIKE 'Merv Griffin');
+		    FROM movieexec
+		    WHERE name LIKE 'Merv Griffin');
+		    
