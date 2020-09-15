d7d6755d1e0504fada959268cbe037d2a67955f5
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-09-15 22:10:02 +0300

    Update Solutions PC.sql

--- Exercises/Chapter 3/Solutions PC.sql
+++ Exercises/Chapter 3/Solutions PC.sql
@@ -10,38 +10,38 @@ WHERE p.speed > 500;
 SELECT code, model, price
 FROM printer
 WHERE price >= ALL(SELECT price
-				   FROM printer);
+		   FROM printer);
 
 --3
 SELECT *
 FROM laptop
 WHERE speed < ALL(SELECT speed
-				  FROM pc);
+		  FROM pc);
 
 --4
 SELECT model, price
 FROM laptop
 WHERE price >= ALL(SELECT price FROM laptop
-				   UNION
-				   SELECT price FROM pc
-				   UNION
-				   SELECT price FROM printer)
+		   UNION
+		   SELECT price FROM pc
+		   UNION
+		   SELECT price FROM printer)
 UNION
 SELECT model, price
 FROM pc
 WHERE price >= ALL(SELECT price FROM laptop
-				   UNION
-				   SELECT price FROM pc
-				   UNION
-				   SELECT price FROM printer)
+	           UNION
+		   SELECT price FROM pc
+		   UNION
+		   SELECT price FROM printer)
 UNION
 SELECT model, price
 FROM printer
 WHERE price >= ALL(SELECT price FROM laptop
-				   UNION
-				   SELECT price FROM pc
-				   UNION
-				   SELECT price FROM printer);
+		   UNION
+		   SELECT price FROM pc
+		   UNION
+		   SELECT price FROM printer);
 
 --5
 SELECT p.maker
@@ -49,8 +49,8 @@ FROM product p
 JOIN printer pr ON p.model = pr.model
 WHERE pr.color LIKE 'y'
 AND pr.price <= ALL(SELECT price
-					FROM printer
-					WHERE color like 'y');
+		    FROM printer
+		    WHERE color like 'y');
 
 --6
 SELECT DISTINCT pr.maker
