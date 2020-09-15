fdd6590710ecefb3e0528b77c8f9961831aaf716
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-09-15 22:12:14 +0300

    Update Solutions Ships.sql

--- Exercises/Chapter 3/Solutions Ships.sql
+++ Exercises/Chapter 3/Solutions Ships.sql
@@ -4,35 +4,35 @@ USE ships
 SELECT DISTINCT country
 FROM classes
 WHERE numguns >= ALL(SELECT numguns
-					 FROM CLASSES);
+		     FROM CLASSES);
 
 --2
 SELECT DISTINCT class 
 FROM ships
 WHERE name in (SELECT ship
-			   FROM outcomes
-			   WHERE result LIKE 'sunk');
+	       FROM outcomes
+	       WHERE result LIKE 'sunk');
 
 --3
 SELECT name, class
 FROM ships
 WHERE class IN (SELECT class
-				FROM classes
-				WHERE bore=16);
+		FROM classes
+		WHERE bore=16);
 
 --4
 SELECT battle
 FROM outcomes
 WHERE ship IN (SELECT name
-			   FROM ships
-			   WHERE class LIKE 'Kongo');  
+	       FROM ships
+	       WHERE class LIKE 'Kongo');  
 
 --5
 SELECT class, name
 FROM ships
 WHERE class IN (SELECT class
-				FROM classes c1
-				WHERE numguns >= ALL(SELECT numguns
-									 FROM classes c2
-									 WHERE c1.bore = c2.bore) )
+		FROM classes c1
+		WHERE numguns >= ALL(SELECT numguns
+				     FROM classes c2
+				     WHERE c1.bore = c2.bore) )
 ORDER BY class;
