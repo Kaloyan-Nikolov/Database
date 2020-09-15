a592fda0281f20a3976e131aa975fa82ff775688
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-09-15 22:17:29 +0300

    Update Solutions Movies.sql

--- Exercises/Chapter 4/Solutions Movies.sql
+++ Exercises/Chapter 4/Solutions Movies.sql
@@ -5,8 +5,8 @@ SELECT m.title, e.name
 FROM movie m
 JOIN movieexec e ON m.producerc# = e.cert#
 WHERE m.producerc# IN (SELECT producerc#
-				   FROM movie
-				   WHERE title LIKE 'Star Wars');
+		       FROM movie
+		       WHERE title LIKE 'Star Wars');
 
 --2
 SELECT DISTINCT e.name
@@ -27,7 +27,7 @@ FROM movieexec e
 JOIN movie m ON m.producerc# = e.cert#
 JOIN starsin s ON m.title LIKE s.movietitle
 WHERE e.networth >= ALL(SELECT networth
-						FROM movieexec);
+			FROM movieexec);
 
 --5
 SELECT m.name, s.movietitle
