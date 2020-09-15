ad33a73f3aa57351949dcbec0d00ad7fea38fa36
Kaloyan Nikolov <60822878+Kaloyan-Nikolov@users.noreply.github.com>
2020-09-15 21:58:39 +0300

    Update PC.sql

--- Exercises/Creating the database/PC.sql
+++ Exercises/Creating the database/PC.sql
@@ -56,7 +56,7 @@ ALTER TABLE pc ADD	CONSTRAINT FK_pc_product FOREIGN KEY(model) REFERENCES produc
 
 ALTER TABLE printer ADD	CONSTRAINT FK_printer_product FOREIGN KEY(model) REFERENCES product(model);
 
-----Product------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+-----Product----- 
 insert into product values('B','1121','PC');
 insert into product values('A','1232','PC');
 insert into product values('A','1233','PC');
@@ -73,7 +73,7 @@ insert into product values('B','1750','Laptop');
 insert into product values('A','1752','Laptop');
 insert into product values('E','2111','PC');
 insert into product values('E','2112','PC');
-----PC------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+-----PC----- 
 insert into pc values(1,'1232',500,64,5,'12x',600);
 insert into pc values(2,'1121',750,128,14,'40x',850);
 insert into pc values(3,'1233',500,64,5,'12x',600);
@@ -85,14 +85,14 @@ insert into pc values(8,'1232',450,64,8,'24x',350);
 insert into pc values(9,'1232',450,32,10,'24x',350);
 insert into pc values(10,'1260',500,32,10,'12x',350);
 insert into pc values(11,'1233',900,128,40,'40x',980);
-----Laptop------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+-----Laptop----- 
 insert into laptop values(1,'1298',350,32,4,700,11);
 insert into laptop values(2,'1321',500,64,8,970,12);
 insert into laptop values(3,'1750',750,128,12,1200,14);
 insert into laptop values(4,'1298',600,64,10,1050,15);
 insert into laptop values(5,'1752',750,128,10,1150,14);
 insert into laptop values(6,'1298',450,64,10,950,12);
-----Printer------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+-----Printer----- 
 insert into printer values(1,'1276','n','Laser',400);
 insert into printer values(2,'1433','y','Jet',270);
 insert into printer values(3,'1434','y','Jet',290);
